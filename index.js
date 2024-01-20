d75de38421d6a71d004e913c94bbb4613511f8fd
Elsaigh <elsaigh1@outlook.com>
2024-01-20 07:55:41 -0500

    Add delete functionality to remove an article by delete button

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import { v4 as uuidv4 } from 'uuid'
 
 const app = express()
 const port = 3000
+
+var articleTexts = []
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
 
-app.post('/', (req, res) => {
-  res.render('index.ejs')
+app.post('/create', (req, res) => {
+  console.log('Method: ' + req.method)
+  console.log(req.body)
+
+  const articleText = req.body['articleText']
+
+  articleTexts.push(articleText)
+  res.render('index.ejs', {
+    articleTexts: articleTexts
+  })
 })
+
+app.post('/delete', (req, res) => {
+  const deletionIndex = req.body['articleIndex']
+
+  articleTexts.splice(deletionIndex, 1);
+
+  console.log(articleTexts)
+  res.render('index.ejs', {
+    articleTexts: articleTexts
+  })
+})
+
+// app.post('/edit', (req, res) => {
+//   const deletionIndex = req.body['articleIndex']
+
+//   articleTexts.splice(deletionIndex, 1);
+
+//   console.log(articleTexts)
+//   res.render('index.ejs', {
+//     articleTexts: articleTexts
+//   })
+// })
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`)
